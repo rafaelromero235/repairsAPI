217b0d9ec4b5df17f55efdb252b70facde7fcbc1
rafael romero <rafaelromero235@gmail.com>
2023-01-28 20:35:22 -0400

    primer commit .

--- database/db.js
+++ database/db.js
@@ -5,7 +5,7 @@ const db = new Sequelize({
   host: 'localhost',
   port: 5433,
   username: 'postgres',
-  password: 'root',
+  password: 'root', //por alguna razon no me deja usar la variable de entorno
   database: 'reapairsDB',
 
   logging: false,
